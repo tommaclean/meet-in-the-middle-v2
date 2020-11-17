7d2ebfb9826709c2ebdac83cd2580271f3e21e44
tommaclean <tom.j.maclean@gmail.com>
2020-11-17 18:20:34 -0500

    added MainPage

---
+++ src/containers/MainPage/MainPage.js
@@ -0,0 +1,13 @@
+import React from 'react'
+import './MainPage.css'
+
+function MainPage() {
+    return (
+        <div className="MainPage-header">
+            This is the main page.
+        </div>
+    )
+}
+
+
+export default MainPage;
