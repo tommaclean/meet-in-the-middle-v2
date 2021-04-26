dfa8e9760ea5aafa9b77918e476ac1c44d4cd1ad
tommaclean <tom.j.maclean@gmail.com>
2021-04-26 16:11:57 -0400

    removed spinner timeout

--- src/components/UI/Spinner/Spinner.js
+++ src/components/UI/Spinner/Spinner.js
@@ -3,7 +3,7 @@ import Loader from "react-loader-spinner";
 
 const Spinner = () => {
     return ( 
-            <Loader type="Rings" color="#00BFFF" height={250} width={250} timeout={3000}/>
+            <Loader type="Rings" color="#00BFFF" height={250} width={250} />
     );
 }
 
